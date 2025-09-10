73d0cbe0f3a614d5082aeec604a4d0ef3b880c89
127NCopernico <127NCopernico@gmail.com>
2025-09-10 09:25:40 -0400

    logic - win

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
-public class Main {
-    int[] board = new int[9];
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    public static Scanner sc = new Scanner(System.in);
+    public static int[] board = new int[9];
+    public static void main(String[] args) {
+        boolean xturn = true;
+        while(true){
+            if(xturn){
+                System.out.print("X, ");
+            }else{
+                System.out.print("O, ");
+            }
+            System.out.print("Enter board pos: ");
+            int n = sc.nextInt();
+            while (n < 1 || n > 9 || board[n] != 0){
+                System.out.print("Invalid, enter board pos: ");
+                n = sc.nextInt();
+            }
+            if(xturn){
+                board[n] = 1;
+            }else{
+                board[n] = 2;
+            }
+            xturn = !xturn;
+        }
     }
 }
